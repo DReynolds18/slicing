2412a70f45559a66d309bc21bebabc3e879ed6bf
“Dylan <dreynolds18@avc.edu>
2022-09-19 20:12:10 -0700

    Assignment for Slicing

---
+++ slicing.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 18:04:06 2022
+
+@author: dreynolds18
+
+problem:
+    
+    Use numpy.array to define a 3-by-7 array, A.  In array location (i,j), row i, column j,  put the number i*j
+
+    Use slicing to produce and print:
+
+    1)  row 2 of A
+
+    2) row 2 of A in reverse order
+
+    3) the sub-matrix consisting or the bottom right 2x2 sub-array 
+
+    4) the sub-array consisting of the bottom left 2x2 sub-array
+
+    5) the 3x3 sub-array in the middle of A
+"""
+
+import numpy as np
+
+A = np.zeros(shape = (3,7))
+
+for i in range(3):
+    for j in range(7):
+        A[i,j] = i*j
+
+## 1)  row 2 of A
+print(A[2,:])
+
+## 2) row 2 of A in reverse order
+print(A[2,::-1])
+
+## 3) the sub-matrix consisting or the bottom right 2x2 sub-array
+print(A[1:,5:])
+
+## 4) the sub-array consisting of the bottom left 2x2 sub-array
+print(A[1:,:2])
+
+## 5) the 3x3 sub-array in the middle of A
+print(A[:,2:5])
